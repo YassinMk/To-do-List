14109cf82ce94267e8a6d908ed49f222eb3a5101
YassinMk <122708120+YassinMk@users.noreply.github.com>
2024-03-06 00:46:11 +0000

    Add task form with title, description, and buttons

--- src/Component/CreateTaskFormule.jsx
+++ src/Component/CreateTaskFormule.jsx
@@ -4,9 +4,11 @@ import { Modal as BaseModal } from "@mui/base/Modal";
 import PropTypes from "prop-types";
 import clsx from "clsx";
 import Paper from "@mui/material/Paper";
+import TextField from "@mui/material/TextField";
+import { Stack, Typography } from "@mui/material";
+import Button from "@mui/material/Button";
 
 const CreateTaskFormule = ({ open = false, handleClose }) => {
-
   return (
     <Modal
       aria-labelledby="unstyled-modal-title"
@@ -16,12 +18,39 @@ const CreateTaskFormule = ({ open = false, handleClose }) => {
       slots={{ backdrop: StyledBackdrop }}
     >
       <Paper sx={{ width: 500, padding: "1.5em" }} p={2}>
-        <h2 id="unstyled-modal-title" className="modal-title">
-          Text in a modal
-        </h2>
-        <p id="unstyled-modal-description" className="modal-description">
-          Aliquid amet deserunt earum!
-        </p>
+        <Stack spacing={3}>
+          <Typography textAlign={"center"} variant="h5">Add Task</Typography>
+          <TextField
+            id="standard-basic"
+            label="Title of task"
+            variant="standard"
+            sx={textFieldStyles}
+            
+          />
+          <TextField
+            id="standard-basic"
+            label="Description"
+            variant="standard"
+            sx={{
+              ...textFieldStyles,
+              "& .MuiInputBase-root": {
+                // Target the Input component
+                fontFamily: "Rubik, sans-serif", // Set the font family
+              },
+              '& label.Mui-focused': {
+                color: 'rgb(25, 118, 210,0.8)', // Change the color of the label when the TextField is focused
+              },
+              '& .MuiInput-underline:after': {
+                borderBottomColor: 'rgb(25, 118, 210,0.8)', // Change the underline color when the TextField is focused
+              }
+            }}
+            
+          />
+          <Stack flexDirection={"row"} gap={2}>
+            <Button variant="text"sx={{fontFamily:"Rubik, sans-serif"}} >Confirm</Button>
+            <Button variant="text"sx={{fontFamily:"Rubik, sans-serif", }}  color="error" onClick={handleClose}>Cancel</Button>
+          </Stack>
+        </Stack>
       </Paper>
     </Modal>
   );
@@ -59,4 +88,13 @@ const StyledBackdrop = styled(Backdrop)`
   -webkit-tap-highlight-color: transparent;
 `;
 
-export default CreateTaskFormule
+
+const textFieldStyles = {
+  color:"rgb(37, 47, 136)",
+  "& .MuiInputLabel-root": {
+    // Target the InputLabel component
+    fontFamily: "Rubik, sans-serif", // Set the font family
+  }
+};
+
+export default CreateTaskFormule;
