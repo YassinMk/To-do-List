ff976eb5b81925def895ea79bb8091662324df40
YassinMk <122708120+YassinMk@users.noreply.github.com>
2024-03-31 12:41:01 +0000

    Add scrollbar to TasksCompleted component

--- src/Component/TasksCompleted.jsx
+++ src/Component/TasksCompleted.jsx
@@ -1,12 +1,65 @@
 import Task from "./Task";
+import { Box } from "@mui/system";
+import useFetch from "../FetchHook/useFetch";
+import api from "../Api/apiCall";
+import CircularProgress from "@mui/material/CircularProgress";
+import { Typography } from "@mui/material";
+
+
 const TasksCompleted = () => {
+  const { data: tasksCompleted, isPending, error } = useFetch(api.getComplete());
+
     return ( 
-        <>
-        <Task />
-    
-        </>
+        <Box
+        sx={scrollbarStyle}
+      >
+        {
+          isPending && <CircularProgress sx={{ height: "25%" }} />
+        }
+        {
+          error && (
+            <Typography
+              align="center"
+              variant="h5"
+              sx={{ fontFamily: "'Rubik', sans-serif", marginTop: "25%" }}
+            >
+              {error}
+            </Typography>
+          )
+        }
+        {
+          tasksCompleted && tasksCompleted.map((taskCp) => <Task key={taskCp.id} task={taskCp} completed={taskCp.completed} />)
+        }
+        </Box>
         
      );
+}
+
+
+
+
+const scrollbarStyle={
+  
+    width: "100%",
+    justifyContent: "center",
+    alignItems: "center",
+    paddingLeft: "2em",
+    overflowY: "auto",
+    height: "350px",
+    marginTop: ".5em",
+    "&::-webkit-scrollbar": {
+      width: "8px",
+    },
+    "&::-webkit-scrollbar-track": {
+      background: "#f1f1f1",
+    },
+    "&::-webkit-scrollbar-thumb": {
+      background: "#7a9efa",
+      borderRadius: "5px",
+    },
+    "&::-webkit-scrollbar-thumb:hover": {
+      background: "#7b9efb",
+    },
 }
  
 export default TasksCompleted;
