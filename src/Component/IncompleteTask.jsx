31ad4e4c4bdc1c07386baf967e858c706dd0c6b5
YassinMk <122708120+YassinMk@users.noreply.github.com>
2024-03-31 12:40:45 +0000

    Add incomplete task list and styling

--- src/Component/IncompleteTask.jsx
+++ src/Component/IncompleteTask.jsx
@@ -1,12 +1,40 @@
 import { Typography } from "@mui/material";
 import { useState } from "react";
 import { Box } from "@mui/system";
+import Task from "./Task";
+import useFetch from "../FetchHook/useFetch";
+import api from "../Api/apiCall";
+import CircularProgress from "@mui/material/CircularProgress";
+
+const scrollbarStyle = {
+  width: "100%",
+  justifyContent: "center",
+  alignItems: "center",
+  paddingLeft: "2em",
+  overflowY: "auto",
+  height: "350px",
+  marginTop: ".5em",
+  "&::-webkit-scrollbar": {
+    width: "8px",
+  },
+  "&::-webkit-scrollbar-track": {
+    background: "#f1f1f1",
+  },
+  "&::-webkit-scrollbar-thumb": {
+    background: "#7a9efa",
+    borderRadius: "5px",
+  },
+  "&::-webkit-scrollbar-thumb:hover": {
+    background: "#7b9efb",
+  },
+};
 
 const IncompleteTask = () => {
-  const [incompleteTask, setIncompleteTask] = useState([]);
+  const { data: incompleteTasks, isPending, error } = useFetch(api.getNotComplete());
+
   return (
     <>
-      {incompleteTask.length === 0 ? (
+      {incompleteTasks && incompleteTasks.length === 0 ? (
         <Box
           sx={{
             display: "flex",
@@ -21,12 +49,24 @@ const IncompleteTask = () => {
             fontFamily={"Rubik, sans-serif"}
             fontWeight={200}
           >
-            You  complete all the tasks! Good Job 👍
+            You complete all the tasks! Good Job 👍
           </Typography>
         </Box>
-        
       ) : (
-        <p>fdsfdfds</p>
+        <Box sx={scrollbarStyle}>
+          {isPending && <CircularProgress sx={{ height: "25%" }} />}
+          {error && (
+            <Typography
+              align="center"
+              variant="h5"
+              sx={{ fontFamily: "'Rubik', sans-serif", marginTop: "25%" }}
+            >
+              {error}
+            </Typography>
+          )}
+          {incompleteTasks &&
+            incompleteTasks.map((task) => <Task key={task.id} task={task} completed={task.completed} />)}
+        </Box>
       )}
     </>
   );
