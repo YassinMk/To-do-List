72077241724f4ea96ded6c3d58faeeac9eb42396
YassinMk <122708120+YassinMk@users.noreply.github.com>
2024-04-03 12:11:01 +0000

    Add TaskProvider and useDeleteTask hook

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
 import Stack from "@mui/material/Stack";
 import "./Index.css";
 import TodoList from "./Component/TodoList";
+import { TaskProvider } from "./Context/TasksContext.js";
+
+
+
 
 
 function App() {
+
   return (
-    <Stack
-      direction="column"
-      justifyContent="center"
-      alignItems="center"
-      sx={{ height: "97vh" }}
-    >
-      <TodoList />
-    </Stack>
+    <TaskProvider>
+      <Stack
+        direction="column"
+        justifyContent="center"
+        alignItems="center"
+        sx={{ height: "97vh" }}
+      >
+        <TodoList />
+      </Stack>
+    </TaskProvider>
     
   );
 }
---
+++ src/Context/TasksContext.js
@@ -0,0 +1,27 @@
+import React, { createContext, useContext, useState } from 'react';
+
+export const TaskContext = createContext();
+
+export const TaskProvider = ({ children }) => {
+  const [deletedTaskId, setDeletedTaskId] = useState(null);
+
+  const deleteTask = (id) => {
+    setDeletedTaskId(id);
+  };
+
+  return (
+    <TaskContext.Provider value={{ deletedTaskId, deleteTask }}>
+      {children}
+    </TaskContext.Provider>
+  );
+};
+
+export const useDeleteTask = () => {
+  const context = useContext(TaskContext);
+
+  if (context === undefined) {
+    throw new Error('useDeleteTask must be used within a DeleteTaskProvider');
+  }
+
+  return context;
+};
