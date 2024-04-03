5a420016ac48cc648bd633c222bf0c33283b69b8
YassinMk <122708120+YassinMk@users.noreply.github.com>
2024-04-03 13:05:28 +0000

    Refactor TasksCompleted component and add error handling

--- src/Component/TasksCompleted.jsx
+++ src/Component/TasksCompleted.jsx
@@ -3,9 +3,9 @@ import { Box } from "@mui/system";
 import useFetch from "../FetchHook/useFetch";
 import api from "../Api/apiCall";
 import CircularProgress from "@mui/material/CircularProgress";
-import { Typography } from "@mui/material";
 import { useDeleteTask } from "../Context/TasksContext";
-
+import { Alert } from "@mui/material";
+import { scrollbarStyle } from "./style.js";
 
 const TasksCompleted = () => {
   const { data: tasks, isPending, error } = useFetch(api.getComplete());
@@ -14,60 +14,28 @@ const TasksCompleted = () => {
   let tasksCompleted = [];
   if (deletedTaskId && tasks) {
     tasksCompleted = tasks.filter((task) => task.id !== deletedTaskId);
-  }else{
-    tasksCompleted=tasks;
+  } else {
+    tasksCompleted = tasks;
   }
-    return ( 
-        <Box
-        sx={scrollbarStyle}
-      >
-        {
-          isPending && <CircularProgress sx={{ height: "25%" }} />
-        }
-        {
-          error && (
-            <Typography
-              align="center"
-              variant="h5"
-              sx={{ fontFamily: "'Rubik', sans-serif", marginTop: "25%" }}
-            >
-              {error}
-            </Typography>
-          )
-        }
-        {
-          tasksCompleted && tasksCompleted.map((taskCp) => <Task key={taskCp.id} task={taskCp} completed={taskCp.completed} />)
-        }
-        </Box>
-        
-     );
-}
+  return (
+    <Box sx={scrollbarStyle}>
+      {isPending && <CircularProgress sx={{ height: "25%" }} />}
+      {error && (
+        <Alert
+          sx={{ width: "65%", marginTop: "20%", marginLeft: "5em" }}
+          severity="error"
+        >
+          {error}
+        </Alert>
+      )}
+      {tasksCompleted &&
+        tasksCompleted.map((taskCp) => (
+          <Task key={taskCp.id} task={taskCp} completed={taskCp.completed} />
+        ))}
+    </Box>
+  );
+};
 
 
 
-
-const scrollbarStyle={
-  
-    width: "100%",
-    justifyContent: "center",
-    alignItems: "center",
-    paddingLeft: "2em",
-    overflowY: "auto",
-    height: "350px",
-    marginTop: ".5em",
-    "&::-webkit-scrollbar": {
-      width: "8px",
-    },
-    "&::-webkit-scrollbar-track": {
-      background: "#f1f1f1",
-    },
-    "&::-webkit-scrollbar-thumb": {
-      background: "#7a9efa",
-      borderRadius: "5px",
-    },
-    "&::-webkit-scrollbar-thumb:hover": {
-      background: "#7b9efb",
-    },
-}
- 
 export default TasksCompleted;
